b16d2cd04b1127ae50497b75a7bc483c35d03be8
Sirichat Pannoi <sirichat2003@gmail.com>
2024-09-01 12:55:39 +0700

    Exchange Currency

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:simple_application/screens/latestRate.dart';
 import 'package:simple_application/screens/convert.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "simple app",
+    title: "Exchange Currency",
     home: Scaffold(
       appBar: AppBar(
-        title: const Text("Simple App"),
+        title: const Text("Exchange Currency"),
       ),
-      body: const Convert(),
+      body: const LatestRate(),
     ),
   ));
 }
--- lib/screens/convert.dart
+++ lib/screens/convert.dart
@@ -1,54 +1,63 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:simple_application/Currency.dart';
 
 class Convert extends StatefulWidget {
   const Convert({super.key});
-
   @override
   State<Convert> createState() => _ConvertState();
 }
 
 class _ConvertState extends State<Convert> {
-
   @override
-  void initState() {
+  initState() {
     super.initState();
-    Future<Currency> result = getCurrency();
+    print("Init State");
   }
 
-  Future<Currency> getCurrency() async{
-    // var params = <String, String>{
-    //   'base': 'USD'
-    // };
-    var url = "currency-converter-pro1.p.rapidapi.com";
+  Future<Currency> getCurrency() async {
+    var uri =
+        Uri.parse("https://currency-converter-pro1.p.rapidapi.com/currencies");
 
-    var uri = Uri.https(url,'/currencies');
-
-    var response = await http.get(
-      uri,
-      headers: <String, String>{
-        'X-RapidAPI-Key': '620beb11b3msh657fac4af7a0357p16c61ajsnb1c4b110030a',
-        'X-RapidAPI-Host': 'currency-converter-pro1.p.rapidapi.com'
-      },
-    );
-    Currency currency = currencyFromJson(response.body);
-    return currency;
-    
-    // return result;
-
+    var response = await http.get(uri, headers: {
+      "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com",
+      "x-rapidapi-key": "C05GBKbyg0mshuD0FmkuTgEJe7mQp1QktrHjsndVBQ98Lef6WS"
+    });
+    Currency result = currencyFromJson(response.body);
+    print(result.toString());
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
+    print("Build");
     return FutureBuilder(
       future: getCurrency(),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if(snapshot.connectionState == ConnectionState.done){
-          return Text("Finished Load Data"); 
+      builder: (BuildContext context, AsyncSnapshot<Currency> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.result!.length,
+              itemBuilder: (BuildContext context, int index) {
+                String? key = snapshot.data?.result!.keys.elementAt(index);
+                String? value = snapshot.data?.result![key];
+                return ListTile(
+                  title: Text(key!),
+                  subtitle: Text(value!),
+                );
+              }
+            );
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
-        return CircularProgressIndicator();
-      });
+      },
+    );
   }
 }
